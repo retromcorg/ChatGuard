30c7cdeca13a6c2613707fce9e09a1a48565c15a
AleksandarHaralanov <aleksandar.haralanov01@gmail.com>
2025-01-20 23:46:09 +0200

    Shorten chat listener to use handler classes instead

--- src/main/java/io/github/aleksandarharalanov/chatguard/listener/PlayerChatListener.java
+++ src/main/java/io/github/aleksandarharalanov/chatguard/listener/PlayerChatListener.java
@@ -2,16 +2,16 @@ package io.github.aleksandarharalanov.chatguard.listener;
 
 import com.earth2me.essentials.Essentials;
 import com.earth2me.essentials.User;
-import org.bukkit.entity.Player;
 import org.bukkit.event.player.PlayerChatEvent;
 import org.bukkit.event.player.PlayerListener;
 
 import java.util.Arrays;
-import java.util.HashMap;
 
 import static io.github.aleksandarharalanov.chatguard.ChatGuard.getConfig;
-import static io.github.aleksandarharalanov.chatguard.ChatGuard.getStrikes;
-import static io.github.aleksandarharalanov.chatguard.handler.MessageHandler.checkMessage;
+import static io.github.aleksandarharalanov.chatguard.handler.CaptchaHandler.checkPlayerCaptcha;
+import static io.github.aleksandarharalanov.chatguard.handler.CaptchaHandler.isPlayerCaptchaActive;
+import static io.github.aleksandarharalanov.chatguard.handler.FilterHandler.checkPlayerMessage;
+import static io.github.aleksandarharalanov.chatguard.handler.SpamHandler.isPlayerSpamming;
 import static io.github.aleksandarharalanov.chatguard.util.AccessUtil.hasPermission;
 import static io.github.aleksandarharalanov.chatguard.util.ColorUtil.translate;
 import static io.github.aleksandarharalanov.chatguard.util.LoggerUtil.logWarning;
@@ -19,15 +19,11 @@ import static org.bukkit.Bukkit.getServer;
 
 public class PlayerChatListener extends PlayerListener {
 
-    private static final HashMap<String, Long> playerTimestamps = new HashMap<>();
-
     @Override
-    public void onPlayerChat(PlayerChatEvent event) {
-        Player player = event.getPlayer();
-
+    public void onPlayerChat(final PlayerChatEvent event) {
         Essentials essentials = (Essentials) getServer().getPluginManager().getPlugin("Essentials");
         if (essentials != null && essentials.isEnabled()) {
-            User user = essentials.getUser(player.getName());
+            User user = essentials.getUser(event.getPlayer().getName());
             if (user.isMuted()) {
                 event.setCancelled(true);
 
@@ -52,42 +48,30 @@ public class PlayerChatListener extends PlayerListener {
                     timeMessage.append(seconds).append(" second(s)");
                 }
 
-                player.sendMessage(translate(timeMessage.toString()));
+                event.getPlayer().sendMessage(translate(timeMessage.toString()));
                 return;
             }
         }
 
-        if (hasPermission(player, "chatguard.bypass")) return;
+        if (hasPermission(event.getPlayer(), "chatguard.bypass")) return;
+
+        boolean isCaptchaEnabled = getConfig().getBoolean("captcha.enabled", true);
+        if (isCaptchaEnabled)
+            if (isPlayerCaptchaActive(event)) return;
 
         boolean isSpamPreventionEnabled = getConfig().getBoolean("spam-prevention.enabled", true);
-        if (isSpamPreventionEnabled) {
-            long timestamp = System.currentTimeMillis();
-            Long lastTimestamp = playerTimestamps.get(player.getName());
-            if (lastTimestamp != null) {
-                long elapsed = timestamp - lastTimestamp;
-                int strikeTier = getStrikes().getInt(player.getName(), 0);
-                int cooldown = getConfig().getInt(String.format("spam-prevention.cooldown-ms.s%d", strikeTier), 0);
-
-                if (elapsed <= cooldown) {
-                    boolean isWarnEnabled = getConfig().getBoolean("spam-prevention.warn-player", true);
-                    if (isWarnEnabled) {
-                        double remainingTime = (cooldown - elapsed) / 1000.0;
-                        player.sendMessage(translate(String.format("&cPlease wait %.2f sec. before sending another message.", remainingTime)));
-                    }
-                    event.setCancelled(true);
-                    return;
-                }
-            }
-            playerTimestamps.put(player.getName(), timestamp);
-        }
+        if (isSpamPreventionEnabled)
+            if (isPlayerSpamming(event)) return;
 
         boolean isFilterEnabled = getConfig().getBoolean("filter.enabled", true);
         if (isFilterEnabled) {
             try {
-                checkMessage(event);
+                checkPlayerMessage(event);
             } catch (Exception e) {
                 logWarning(Arrays.toString(e.getStackTrace()));
             }
         }
+
+        if (isCaptchaEnabled) checkPlayerCaptcha(event);
     }
 }
