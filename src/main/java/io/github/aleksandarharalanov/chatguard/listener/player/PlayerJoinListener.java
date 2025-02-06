96299c7b1db366b30f613316d54ff5967846ba8a
AleksandarHaralanov <aleksandar.haralanov01@gmail.com>
2025-02-06 17:25:26 +0200

    Prevent strike config from generating a property if the username should be blocked, add console log for kick

--- src/main/java/io/github/aleksandarharalanov/chatguard/listener/player/PlayerJoinListener.java
+++ src/main/java/io/github/aleksandarharalanov/chatguard/listener/player/PlayerJoinListener.java
@@ -1,34 +1,39 @@
 package io.github.aleksandarharalanov.chatguard.listener.player;
 
-import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerListener;
 import org.bukkit.event.player.PlayerPreLoginEvent;
 
 import static io.github.aleksandarharalanov.chatguard.ChatGuard.getStrikes;
-import static io.github.aleksandarharalanov.chatguard.handler.FilterHandler.shouldBlockName;
+import static io.github.aleksandarharalanov.chatguard.handler.FilterHandler.shouldBlockUsername;
+import static io.github.aleksandarharalanov.chatguard.util.ColorUtil.translate;
+import static io.github.aleksandarharalanov.chatguard.util.LoggerUtil.logWarning;
 
 public class PlayerJoinListener extends PlayerListener {
 
     @Override
     public void onPlayerJoin(PlayerJoinEvent event) {
-        String playerName = event.getPlayer().getName();
-        if (getStrikes().getInt(playerName, -1) == -1) {
-            getStrikes().setProperty(playerName, 0);
-            getStrikes().saveConfig();
+        Player player = event.getPlayer();
+        if (shouldBlockUsername(player.getName())) {
+            player.kickPlayer(translate("&cKICKED: Username contains blocked words."));
+            logWarning(String.format("[ChatGuard] Kicked player '%s' for bad username.", player.getName()));
+            return;
         }
 
-        // this may be slightly redundant, but i will add it here just in case the pre-login event somehow fails
-        if (shouldBlockName(playerName)) {
-            event.getPlayer().kickPlayer(ChatColor.RED + "Username contains blacklisted terms.");
+        if (getStrikes().getInt(player.getName(), -1) == -1) {
+            getStrikes().setProperty(player.getName(), 0);
+            getStrikes().save();
         }
     }
 
     @Override
     public void onPlayerPreLogin(PlayerPreLoginEvent event) {
         String playerName = event.getName();
-        if (shouldBlockName(playerName)) {
-            event.disallow(PlayerPreLoginEvent.Result.KICK_BANNED, ChatColor.RED + "Username contains blacklisted terms.");
-        }
+        if (shouldBlockUsername(playerName))
+            event.disallow(
+                    PlayerPreLoginEvent.Result.KICK_BANNED,
+                    translate("&cKICKED: Username contains blocked words.")
+            );
     }
 }
