cbfc834d013bb27464b51ff27350e9022b92f364
RitzKid76 <tjbaker2003@hotmail.com>
2025-04-10 19:16:25 -0400

    remove useless data storage

--- src/main/java/io/github/aleksandarharalanov/chatguard/listener/player/PlayerJoinListener.java
+++ src/main/java/io/github/aleksandarharalanov/chatguard/listener/player/PlayerJoinListener.java
@@ -2,7 +2,6 @@ package io.github.aleksandarharalanov.chatguard.listener.player;
 
 import io.github.aleksandarharalanov.chatguard.core.config.FilterConfig;
 import io.github.aleksandarharalanov.chatguard.core.security.filter.FilterHandler;
-import io.github.aleksandarharalanov.chatguard.core.config.PenaltyConfig;
 import io.github.aleksandarharalanov.chatguard.util.misc.ColorUtil;
 import org.bukkit.entity.Player;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -19,7 +18,5 @@ public class PlayerJoinListener extends PlayerListener {
             player.kickPlayer(ColorUtil.translateColorCodes("&cName contains bad words."));
             return;
         }
-
-        PenaltyConfig.setDefaultStrikeTier(player);
     }
 }
